6b70df845fffba2a901b3fd71e6a5d2f972adbc7
weifong57 <164613462+weifong57@users.noreply.github.com>
2024-03-25 16:50:47 +0800

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -108,8 +108,16 @@ class TestProductRoutes(TestCase):
         data = response.get_json()
         self.assertEqual(data['message'], 'OK')
 
-    # ----------------------------------------------------------
-    # TEST CREATE
+    def test_get_product(self):
+        """It should Get a single Product"""
+        # get the id of a product
+        test_product = self._create_products(1)[0]
+        response = self.client.get(f"{BASE_URL}/{test_product.id}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(data["name"], test_product.name)
+
+  
     # ----------------------------------------------------------
     def test_create_product(self):
         """It should Create a new Product"""
